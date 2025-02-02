ecbb5ab3c3c42c9c4b178b36f4d53824f537794c
felpscirne <cirnefelipe@gmail.com>
2025-02-02 15:37:59 -0300

    Final commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Game from './components/Game'
+
+function App() {
+  return(
+     <>
+      <h1>Jogo da Memória</h1>
+      <Game  />
+    </>
+    );
+}
+
+export default App
